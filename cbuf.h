597f8fcdd96e914d07450229260cb4064f39c9e3
vibhav950 <vibhavtiwari.official@gmail.com>
2025-04-16 00:48:28 +0530

    Fix API reference

--- cbuf.h
+++ cbuf.h
@@ -48,13 +48,13 @@ int cbuf_is_full(cbuf_t *cbuf);
 
 ssize_t cbuf_get_readable_size(cbuf_t *cbuf);
 
-int cbuf_waitfor_readable(cbuf_t *cbuf, size_t nbytes, int64_t timeout_usec);
+int cbuf_waitfor_readable(cbuf_t *cbuf, size_t nbytes, int64_t timeout_msec);
 
 ssize_t cbuf_write_blocking(cbuf_t *cbuf, const uint8_t *buf, size_t nbytes,
-                            int64_t timeout_usec);
+                            int64_t timeout_msec);
 
 ssize_t cbuf_read_blocking(cbuf_t *cbuf, uint8_t *buf, size_t nbytes,
-                           int64_t timeout_usec, bool all);
+                           int64_t timeout_msec, bool all);
 
 ssize_t cbuf_peek(cbuf_t *cbuf, uint8_t *buf, size_t nbytes);
 
